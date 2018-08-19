96e2b79f4613b6323a294126cf6cc2eb6dda30c9
295587514@qq.com <295587514@qq.com>
2018-08-19 12:10:14 +0800

    feat:增加子进程跑爬虫脚本

--- server/crawler/trailer-list.js
+++ server/crawler/trailer-list.js
@@ -2,14 +2,14 @@ const url = 'https://movie.douban.com/tag/#/?sort=R&range=6,10&tags='
 
 const puppeteer = require('puppeteer')
 
-//定义一个定时函数，等待一个time
+//定义一个定时函数，等待一个time后再执行
 const sleep = time => new Promise(resolve => {
     setTimeout(resolve, time)
 })
 
 //一个自动执行的异步函数，来自动获取豆瓣电影上的相关信息
 //这里的内容是豆瓣主页，电影分类的页面
-const temp = (async () => {
+;(async () => {
     console.log ('开始访问目标网页。。。')
 
     //定义一个模拟的chromium浏览器browser，在这上面进行操作
@@ -31,7 +31,7 @@ const temp = (async () => {
     await page.waitForSelector('.more')
 
     //爬取两页的内容
-    for (let i = 0; i<1; i++) {
+    for (let i = 0; i<3; i++) {
         await sleep(3000)
         await page.click('.more')
     }
@@ -44,13 +44,14 @@ const temp = (async () => {
 
         console.log(items)
 
+        //将每个详情push到对象数组links[]中
         if(items.length >= 1){
             items.each((index, item) => {
                 let it = $(item)
-                let doubanId = it.find('div').data('id')
+                let doubanId = it.find('div').data('id') // 这个id对应 data-id="30122633" 
                 let title = it.find('.title').text()
-                let rate = Number(it.find('.rate').text())
-                let poster = it.find('img').attr('src').replace('s_ratio', 'l_ration')
+                let rate = Number(it.find('.rate').text()) // 豆瓣评分是文本，转化为数字
+                let poster = it.find('img').attr('src').replace('s_ratio', 'l_ration')//海报图片的src是小图，替换为大的高清图
 
                 links.push({
                     doubanId,
@@ -64,8 +65,14 @@ const temp = (async () => {
         return links
     })
 
-    browser.close()
+    browser.close() //关闭这个模拟的browser
     console.log(result)
-})
 
-temp()
+    //将这个结果发送出去
+    process.send({result})
+    process.exit(0)
+
+})()
+
+//执行函数代码，不知道为什么视频中的立即执行没有结果，也不报错   。。
+//格式为：  ；(async () => { to do })()原来是缺少括号
---
+++ server/tasks/movie.js
@@ -0,0 +1,34 @@
+const  cp = require('child_process')
+const { resolve } = require('path')
+
+;(async () => {
+    const script = resolve(__dirname, '../crawler/trailer-list') //子进程的脚本
+    const child = cp.fork(script, []) //创建这个子进程
+    let invoked = false   //子进程的标识符
+
+    //错误的处理
+    child.on('error', err => {
+        if(invoked) return
+
+        invoked = true
+
+        console.log(err)
+    })
+
+    //子进程退出返回码
+    child.on('exit', code => {
+        if(invoked) return
+
+        invoked = false
+        let err = code === 0 ? null : new Error('exit code:' + code)
+
+        console.log(err)
+    })
+
+    //监听message消息，得到data，也即子进程返回的结果
+    child.on('message', data => {
+        let result = data.result
+
+        console.log(result)
+    })
+})()
